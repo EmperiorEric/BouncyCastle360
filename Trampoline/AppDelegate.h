38eb7d4459c419a338f29451c904f7b73aeaef30
Ryan Poolos <ryanpoolos@Gmail.com>
2013-09-10 21:14:38 -0600

    Initial Commit

---
+++ Trampoline/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Trampoline
+//
+//  Created by Ryan Poolos on 9/10/13.
+//  Copyright (c) 2013 PopArcade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TrampolineTests/TrampolineTests.m
@@ -0,0 +1,34 @@
+//
+//  TrampolineTests.m
+//  TrampolineTests
+//
+//  Created by Ryan Poolos on 9/10/13.
+//  Copyright (c) 2013 PopArcade. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TrampolineTests : XCTestCase
+
+@end
+
+@implementation TrampolineTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
