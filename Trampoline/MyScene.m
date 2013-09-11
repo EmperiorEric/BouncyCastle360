18d919aa0bc26fae248f149ace9759a8463db3bf
Ryan Poolos <ryanpoolos@Gmail.com>
2013-09-10 22:07:44 -0600

    The ultra basics for a Trampoline game in SpriteKit are in place! holy crap that was easy.

--- Trampoline/MyScene.m
+++ Trampoline/MyScene.m
@@ -8,6 +8,15 @@
 
 #import "MyScene.h"
 
+@interface MyScene ()
+{
+    SKShapeNode *trampoline;
+    SKSpriteNode *player;
+    
+    BOOL touching;
+}
+@end
+
 @implementation MyScene
 
 -(id)initWithSize:(CGSize)size {    
@@ -16,38 +25,55 @@
         
         self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
         
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+        // Initialize our physicsWorld
+        self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
         
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
+        // Create a "Trampoline"
+        trampoline = [SKShapeNode node];
+        [trampoline setFillColor:[SKColor colorWithRed:1.0 green:0.0 blue:0.0 alpha:1.0]];
+        [trampoline setPath:[UIBezierPath bezierPathWithRect:CGRectMake(0.0, 0.0, CGRectGetWidth(self.frame), 50.0)].CGPath];
+        [trampoline setLineWidth:0.0];
         
-        [self addChild:myLabel];
+        trampoline.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(CGRectGetWidth(trampoline.frame), CGRectGetHeight(trampoline.frame))];
+        trampoline.physicsBody.usesPreciseCollisionDetection = YES;
+        trampoline.physicsBody.restitution = 0.99;
+        trampoline.physicsBody.dynamic = NO;
+        
+        [self addChild:trampoline];
+        
+        // Create our Player
+        player = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
+        player.xScale = 0.25;
+        player.yScale = 0.25;
+        player.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
+        
+        player.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:player.size];
+        player.physicsBody.usesPreciseCollisionDetection = YES;
+        
+        [self addChild:player];
+        
+
     }
     return self;
 }
 
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
-    
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
-    }
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    touching = YES;
+//    player.physicsBody.angularDamping = 0.0;
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    touching = NO;
+//    player.physicsBody.angularDamping = 1.0;
 }
 
 -(void)update:(CFTimeInterval)currentTime {
     /* Called before each frame is rendered */
+    if (touching) {
+        player.physicsBody.velocity = CGVectorMake(player.physicsBody.velocity.dx, player.physicsBody.velocity.dy - 10.0);
+    }
 }
 
 @end
