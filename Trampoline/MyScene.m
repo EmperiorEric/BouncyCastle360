ad72e73b77824c76e038b5a80182cba49a23b1b1
Ryan Poolos <ryanpoolos@Gmail.com>
2013-09-10 23:55:40 -0600

    Squeezing and Stretching the player based on speed, very cool. Added some 360iDev themed graphics.

--- Trampoline/MyScene.m
+++ Trampoline/MyScene.m
@@ -12,11 +12,13 @@
 {
     SKLabelNode *heightLabel;
     
-    SKShapeNode *trampoline;
+    SKSpriteNode *trampoline;
     
     SKNode *container;
     
     SKSpriteNode *player;
+    
+    SKSpriteNode *player2;
     
     BOOL touching;
 }
@@ -43,12 +45,10 @@
         container = [SKNode node];
         
         // Create a "Trampoline"
-        trampoline = [SKShapeNode node];
-        [trampoline setFillColor:[SKColor colorWithRed:1.0 green:0.0 blue:0.0 alpha:1.0]];
-        [trampoline setPath:[UIBezierPath bezierPathWithRect:CGRectMake(0.0, 0.0, CGRectGetWidth(self.frame), 24.0)].CGPath];
-        [trampoline setLineWidth:0.0];
+        trampoline = [SKSpriteNode spriteNodeWithImageNamed:@"bouncyCastle"];
+        trampoline.position = CGPointMake(CGRectGetMidX(self.frame), 64.0);
         
-        trampoline.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(CGRectGetWidth(trampoline.frame), CGRectGetHeight(trampoline.frame))];
+        trampoline.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(CGRectGetWidth(trampoline.frame), 20.0)];
         trampoline.physicsBody.usesPreciseCollisionDetection = YES;
         trampoline.physicsBody.restitution = 1.15;
         trampoline.physicsBody.dynamic = NO;
@@ -56,15 +56,22 @@
         [container addChild:trampoline];
         
         // Create our Player
-        player = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        player.xScale = 0.1;
-        player.yScale = 0.1;
+        player = [SKSpriteNode spriteNodeWithImageNamed:@"party_person1"];
+        player.xScale = 0.5;
+        player.yScale = 0.5;
         player.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame) / 2.0);
         
         player.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:player.size];
         player.physicsBody.usesPreciseCollisionDetection = YES;
         
         [container addChild:player];
+        
+        player2 = [SKSpriteNode spriteNodeWithImageNamed:@"party_person2"];
+        player2.xScale = 0.5;
+        player2.yScale = 0.5;
+        player2.position = CGPointMake(CGRectGetMidX(self.frame), 1000.0);
+        
+        [container addChild:player2];
         
         [self addChild:container];
     }
@@ -85,22 +92,33 @@
 //    [self playerCoasting];
 }
 
--(void)update:(CFTimeInterval)currentTime {
-    
+-(void)update:(CFTimeInterval)currentTime
+{
+    // Update Height Label
     [heightLabel setText:[NSString stringWithFormat:@"%.0f",player.position.y]];
     
-    /* Called before each frame is rendered */
+    // Adjust Velocity based on touch.
     if (touching) {
         player.physicsBody.velocity = CGVectorMake(player.physicsBody.velocity.dx, player.physicsBody.velocity.dy + self.physicsWorld.gravity.dy);
     }
     
+    // Visually adjust player based on velocity
+    CGFloat speed = fabs(player.physicsBody.velocity.dy);
+    // squeeze the player on the x the faster they go.
+    player.xScale = MAX(MIN(0.5, 500.0/speed), 0.25);
+    // Strech the player on the y the faster they go.
+    player.yScale = MAX(MIN(0.75, speed/2000.0), 0.5);
+    
+    NSLog(@"xScale: %f yScale: %f",1000.0/speed,speed/1000.0);
+    
+    // Keep player in the center of the screen
     CGFloat threshold = 320.0;
     
     CGFloat offset = MAX((player.position.y - threshold), 0.0);
     
     [container setPosition:CGPointMake(0.0, -offset)];
     
-    
+    // Flip player when going down
     BOOL goingDown = player.physicsBody.velocity.dy < 0.0;
     
     if (goingDown) {
@@ -108,8 +126,9 @@
     } else {
         player.zRotation = 0.0;
     }
-
-    [self setBackgroundColor:[SKColor colorWithRed:player.frame.origin.y/1500.0 green:0.0 blue:0.0 alpha:1.0]];
+    
+    // Adjust sky color based on altitude
+    [self setBackgroundColor:[SKColor colorWithRed:0.0 green:0.0 blue:5000.0/(player.frame.origin.y * 2.0) alpha:1.0]];
 }
 
 - (void)playerDropping
